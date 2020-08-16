6e400e5f817e09ba299238caf1c5ba1ba26a2bdf
William Schwindt <williammschwindt@gmail.com>
2020-08-16 16:21:25 -0700

    1 done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,23 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+ticket_dict = {}
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    route = []
+    global ticket_dict
+
+    #insert tickets into dict (source: destination)
+    for ticket in tickets:
+        ticket_dict[ticket.source] = ticket.destination
+
+    #append the first flight
+    first_flight = ticket_dict['NONE']
+    route.append(first_flight)
+
+    #lookup the next flight by using the previous flights value 
+    for i in range(1, len(tickets)):
+        route.append(ticket_dict[route[i-1]])
 
     return route
+
