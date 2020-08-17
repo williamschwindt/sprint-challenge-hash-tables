937885c9e806630abc3376b796a124cb915b0e7a
William Schwindt <williammschwindt@gmail.com>
2020-08-16 18:00:01 -0700

    stretch 3/4

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,24 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+weight_dict = {}
 
-    return None
+def get_indices_of_item_weights(weights, length, limit):
+    global weight_dict
+    result = []
+
+    for i in range(0, len(weights) - 1):
+        weight_dict[weights[i]] = i
+
+    for weight in weight_dict:
+        if limit - weight in weight_dict:
+            result = [weight_dict[weight], weight_dict[limit - weight]]
+
+    weight_dict = {}
+
+    if len(result) < 2:
+        return None
+    elif result[0] > result[1]:
+        return (result[0], result[1])
+    else: 
+        return (result[1], result[0])
+    
+
+print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
