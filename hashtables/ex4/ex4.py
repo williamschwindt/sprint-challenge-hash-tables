16cda944d08b676e45e8059c53b868f81be46d0e
William Schwindt <williammschwindt@gmail.com>
2020-08-16 17:10:47 -0700

    mvp complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,22 @@
-def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+number_dict = {}
 
+def has_negatives(a):
+    global number_dict
+    result = []
+
+    for num in a:
+        #if the opposite of the number alrady exists add to result
+        if num - (num*2) in number_dict:
+            result.append(abs(num))
+        elif num not in number_dict:
+            number_dict[num] = num
+        elif num in number_dict:
+            continue
+        
+    number_dict = {}
     return result
 
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    print(has_negatives([1,2,3,-4]))
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,13 +1,29 @@
-# Your code here
-
-
+path_dict = {}
 
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    global path_dict
+    result = []
 
+    for path in files:
+        #find the file name in each path
+        full_path = path
+        path = path.split('/')
+        file_name = path[-1]
+
+        #if the file name already exists the the new path to the list
+        if file_name in path_dict:
+            path_dict[file_name].append(full_path)
+        #other wise add a new entry to path_dict (file_name: [path])
+        else:
+            path_dict[file_name] = [full_path]
+
+    #loop through the query array and add every path that has a file name that = the query
+    for query in queries:
+        if query in path_dict:
+            for file_path in path_dict[query]:
+                result.append(file_path)
+
+    path_dict = {}
     return result
 
 
