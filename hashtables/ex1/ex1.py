45909493d906469058a9711eb428e5c185cbd67c
William Schwindt <williammschwindt@gmail.com>
2020-08-17 17:48:54 -0700

    stretch problem passes all tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,24 +1,24 @@
-weight_dict = {}
-
 def get_indices_of_item_weights(weights, length, limit):
-    global weight_dict
+    weight_dict = {}
     result = []
 
-    for i in range(0, len(weights) - 1):
-        weight_dict[weights[i]] = i
+    for i in range(0, len(weights)):
+        remander = limit - weights[i]
+        if remander in weight_dict:
+            x = weight_dict[remander]
+            result = (i, x)
+        else:
+            weight_dict[weights[i]] = i
 
-    for weight in weight_dict:
-        if limit - weight in weight_dict:
-            result = [weight_dict[weight], weight_dict[limit - weight]]
-
-    weight_dict = {}
+    print(weight_dict)
 
     if len(result) < 2:
         return None
-    elif result[0] > result[1]:
-        return (result[0], result[1])
+    elif i > x:
+        return (i, x)
     else: 
-        return (result[1], result[0])
+        return (x, i)
     
 
-print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
+# print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
+print(get_indices_of_item_weights([4,4], 2, 8))
